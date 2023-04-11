83d0e5d7565e18c580f7309cdf0cb4e221ef46ba
EternalRival <eternalrival8@gmail.com>
2023-04-11 06:14:07 +0300

    feat: transfer to vue

---
+++ src/views/DeployFinder.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+const onSubmit = () => console.log('kek');
+</script>
+
+<template>
+  <main>
+    <form class="p-2 bg-color1 shadow shadow-color4" @submit.prevent="onSubmit">
+      <fieldset class="p-2 flex flex-col items-end gap-1.5">
+        <legend class="font-medium text-lg">Введите данные</legend>
+        <label>
+          Github nickname:
+          <input>
+        </label>
+        <label>
+          Course:
+          <input>
+        </label>
+        <label>
+          Taskname:
+          <input>
+        </label>
+        <button type="submit">Search</button>
+      </fieldset>
+    </form>
+  </main>
+</template>
+
+<style scoped>
+label {
+  @apply flex justify-between items-center;
+}
+
+input {
+  @apply ml-2;
+}
+</style>
