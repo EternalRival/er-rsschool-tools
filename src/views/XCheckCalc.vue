5ecbd12b8cd3e1f25d7df5567b40140dacb3a6da
EternalRival <eternalrival8@gmail.com>
2023-04-15 06:41:56 +0300

    feat: migrate xcheck-calc

--- src/views/XCheckCalc.vue
+++ src/views/XCheckCalc.vue
@@ -1,39 +1,64 @@
 <script setup lang="ts">
-  const onSubmit = () => {};
+  import { computed, ref } from 'vue';
+  const [max, desired] = [ref('100'), ref('0')];
+  const reviewers = ref(['', '', '', '']);
+  const score = computed(() => {
+    const list = reviewers.value
+      .filter((v) => v !== '')
+      .map((v) => +v)
+      .sort((a, b) => b - a)
+      .slice(0, 3);
+    return Math.round(list.reduce((sum, score) => score + sum, 0) / list.length) || 0;
+  });
+  const appeal = computed(() => +desired.value - score.value >= +max.value * 0.1);
+
+  const handleLimits = (e: Event) =>
+    e.target instanceof HTMLInputElement && e.target.value !== '' ? `${Math.min(+e.target.value, +e.target.max)}` : '';
 </script>
 
 <template>
   <main>
-    <form @submit.prevent="onSubmit">
+    <form>
       <fieldset>
         <legend>Введите данные</legend>
         <label>
           Максимум:
-          <input class="w-20" type="number" placeholder="<empty>" />
+          <input
+            :value="max"
+            class="w-20"
+            type="number"
+            placeholder="<empty>"
+            min="0"
+            max="999"
+            @input="(max = handleLimits($event)), $forceUpdate()" />
         </label>
         <label>
           Желаемое:
-          <input class="w-20" type="number" placeholder="<empty>" />
+          <input
+            :value="desired"
+            class="w-20"
+            type="number"
+            placeholder="<empty>"
+            min="0"
+            :max="max"
+            @input="(desired = handleLimits($event)), $forceUpdate()" />
         </label>
-        <label>
-          Reviewer 1:
-          <input class="w-20" type="number" placeholder="<empty>" />
-        </label>
-        <label>
-          Reviewer 2:
-          <input class="w-20" type="number" placeholder="<empty>" />
-        </label>
-        <label>
-          Reviewer 3:
-          <input class="w-20" type="number" placeholder="<empty>" />
-        </label>
-        <label>
-          Reviewer 4:
-          <input class="w-20" type="number" placeholder="<empty>" />
+        <label v-for="(_, index) in reviewers" :key="index">
+          Reviewer {{ index + 1 }}:
+          <input
+            :value="reviewers[index]"
+            class="w-20"
+            type="number"
+            placeholder="<empty>"
+            min="0"
+            :max="max"
+            @input="(reviewers[index] = handleLimits($event)), $forceUpdate()" />
         </label>
       </fieldset>
-      <p></p>
-      <p></p>
+      <p class="text-center">{{ score }}</p>
+      <p :class="`text-center ${appeal ? 'text-color-valid' : 'text-color-invalid'}`">
+        Апелляция {{ appeal ? '' : 'не' }}возможна
+      </p>
     </form>
   </main>
 </template>
