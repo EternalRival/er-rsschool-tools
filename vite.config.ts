c6a99bbad7b366e31a21a4d4247386c4fa611391
EternalRival <eternalrival8@gmail.com>
2023-09-28 14:04:28 +0300

    feat: rename app

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig, UserConfigExport } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-const config: UserConfigExport = defineConfig({ plugins: [vue()], base: '/rsschool-stuff/' });
+const config: UserConfigExport = defineConfig({ plugins: [vue()], base: '/rsschool-tools/' });
 export default config;
