9ed02fcf59f9026ef93739ec4c7706ab9f6aa01e
EternalRival <eternalrival8@gmail.com>
2024-03-13 19:08:22 +0300

    feat: implement debounce

---
+++ src/lib/debounce.ts
@@ -0,0 +1,13 @@
+type Callback<T extends Callback<T>> = (...args: Parameters<T>) => void;
+type Debounce = <T extends Callback<T>>(callback: T, ms: number) => Callback<T>;
+
+export const debounce: Debounce = (callback, ms) => {
+  let timeout: ReturnType<typeof setTimeout>;
+
+  return (...args) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      callback(...args);
+    }, ms);
+  };
+};
