5d497c60c0221530de41d77592d837aeee9cad45
EternalRival <eternalrival8@gmail.com>
2024-03-20 15:40:25 +0300

    perf: optimize deploy url fetching

--- src/shared/api/deploy-finder/lib/fetch-deploy-url.ts
+++ src/shared/api/deploy-finder/lib/fetch-deploy-url.ts
@@ -1,5 +1,3 @@
-import { isString } from '@/shared/lib/type-guards';
-
 import { BASE_URL, REVALIDATE_IN_MINUTES, SUGGESTED_DIRS, SUGGESTED_FILES } from '../model/constants';
 
 import type { ApiRequest } from '../model/request.schema';
@@ -8,13 +6,13 @@ const fetchOptions = { next: { revalidate: REVALIDATE_IN_MINUTES * 60 } };
 
 const buildBaseUrl = ({ course, nickname, task }: ApiRequest): string => `${BASE_URL}${nickname}-${course}/${task}`;
 
-const fetchOkUrl = (url: string): Promise<Nullable<string>> =>
-  fetch(url, fetchOptions).then(({ ok }) => (ok ? url : null));
+const buildSuggestedUrls = (base: string): string[] =>
+  SUGGESTED_DIRS.flatMap((dir) => SUGGESTED_FILES.map((file) => `${base}${dir}${file}`));
 
-const fetchSuggestedUrls = (base: string): Promise<Nullable<string>>[] =>
-  SUGGESTED_DIRS.flatMap((dir) => SUGGESTED_FILES.map((file) => fetchOkUrl(`${base}${dir}${file}`)));
+const mapUrlsToFetches = (urls: string[]): Promise<Response>[] => urls.map((url) => fetch(url, fetchOptions));
 
-const filterOkUrls = (urls: Nullable<string>[]): string[] => urls.filter(isString);
+const parseOkUrls = (responses: Response[]): string[] =>
+  responses.reduce<string[]>((acc, { ok, url }) => (ok ? [...acc, url] : acc), []);
 
-export const findDeployUrls = (deployData: ApiRequest): Promise<string[]> =>
-  Promise.all(fetchSuggestedUrls(buildBaseUrl(deployData))).then(filterOkUrls);
+export const findDeployUrls = async (deployData: ApiRequest): Promise<string[]> =>
+  parseOkUrls(await Promise.all(mapUrlsToFetches(buildSuggestedUrls(buildBaseUrl(deployData)))));
